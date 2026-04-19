5c04cb7d3a55bf5da1cee96d2e7742e36d659c28
Juan <juantg93@hotmail.es>
2026-04-19 15:47:28 +0200

    Eliminacion de linea log en bot_telegram.py. Objetivo: Que no se produzca una linea de log cada vez que se envia una notificacion

--- bot_telegram.py
+++ bot_telegram.py
@@ -23,7 +23,6 @@ def crear_bot():
 def enviar_mensaje_sync(mensaje, chat_id):
     url = f"https://api.telegram.org/bot{TOKEN_TELEGRAM}/sendMessage"
     try:
-        logger.info("Iniciando envio de notificación por telegram...")
         requests.post(url, json={"chat_id": chat_id, "text": mensaje, "parse_mode": "HTML"})
     except Exception as e:
         logger.error(f"Error enviando notificación por Telegram al chat id: {chat_id} ")
