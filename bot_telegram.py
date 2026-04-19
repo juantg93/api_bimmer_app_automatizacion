135e950f1a990f32a4bbbba87b9a99adabc8a5cd
Juan <juantg93@hotmail.es>
2026-04-19 20:48:55 +0200

    Añadido timeout de 10s en el envío de notificación de telegram. Esto evitará errores por falta de respuesta

--- bot_telegram.py
+++ bot_telegram.py
@@ -23,9 +23,14 @@ def crear_bot():
 def enviar_mensaje_sync(mensaje, chat_id):
     url = f"https://api.telegram.org/bot{TOKEN_TELEGRAM}/sendMessage"
     try:
-        requests.post(url, json={"chat_id": chat_id, "text": mensaje, "parse_mode": "HTML"})
+        requests.post(url,
+                      json={"chat_id": chat_id, "text": mensaje, "parse_mode": "HTML"},
+                      timeout=10
+        )
+    except requests.exceptions.Timeout:
+        logger.error(f"Timeout enviando mensaje a chat_id: {chat_id}. Telegram no respondió en 10s.")
     except Exception as e:
-        logger.error(f"Error enviando notificación por Telegram al chat id: {chat_id} ")
+        logger.error(f"Error enviando notificación por Telegram al chat id: {chat_id}: {e} ")
         
 
 
